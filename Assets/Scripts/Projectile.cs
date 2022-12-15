e43af9a7833877462f53172308dff989ecc9ae02
Brauny Nuñez <brayron13@gmail.com>
2022-12-14 23:33:47 -0400

    Code1
    Pr

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    private Monster target;
+
+    private Tower parent;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        MoveToTarget();
+	}
+
+    public void Initialize(Tower parent)
+    {
+        this.target = parent.Target;
+        this.parent = parent;
+    }
+
+    private void MoveToTarget()
+    {
+        if (target != null && target.IsActive) //If the target isn't null and the target isn't dead
+        {
+            //Move towards the position
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, Time.deltaTime * parent.ProjectileSpeed);
+
+            //Calculates the direction of the projectile
+            Vector2 dir = target.transform.position - transform.position;
+
+            //Calculates the angle of the projectile
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+            //Sets the rotation based on the angle
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+        else if (!target.IsActive)
+        {
+            GameManager.Instance.Pool.ReleaseObject(gameObject);
+        }
+    }
+}
